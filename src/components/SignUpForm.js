7395c123b922d00dfae4d4944b459a8d98c1599e
f <f.farokhifar987@gmail.com>
2022-05-19 15:01:51 +0430

    getFieldProps added

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,25 +1,35 @@
+import { useFormik } from "formik";
 const SignUpForm = () => {
+    const formik = useFormik({
+        initialValues:{
+          name: "",
+          email: "",
+          phoneNumber: "",
+          password: "",
+          passwordConfirm: "",
+        },
+    });
   return (
-    <form>
+    <form >
       <div className="formControl">
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" name="name" />
+        <input id="name" type="text" name="name" {...formik.getFieldProps("name")}/>
       </div>
       <div className="formControl">
-        <label>Email</label>
-        <input type="text" name="email" />
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" name="email" {...formik.getFieldProps("email")}/>
       </div>
       <div className="formControl">
-        <label>Phone Number</label>
-        <input type="text" name="phoneNumber" />
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input id="phoneNumber" type="text" name="phoneNumber" {...formik.getFieldProps("phoneNumber")}/>
       </div>
       <div className="formControl">
-        <label>Password</label>
-        <input type="password" name="password" />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" name="password" {...formik.getFieldProps("password")}/>
       </div>
       <div className="formControl">
-        <label>Password Confirm</label>
-        <input type="password" name="passwordConfirm" />
+        <label htmlFor="passwordConfirm">Password Confirm</label>
+        <input id="passwordConfirm" type="password" name="passwordConfirm" {...formik.getFieldProps("passwordConfirm")}/>
       </div>
       <button type="submit">
           Submit
