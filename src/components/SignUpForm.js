0347fcd60e8be979709b9d4da6d2085493f90b88
f <f.farokhifar987@gmail.com>
2022-05-20 13:21:41 +0430

    RadioInput component

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -3,6 +3,11 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import * as Yup from "yup";
 import Input from "./common/Input";
+import RadioInput from "./common/RadioInput";
+const radioOptions = [
+  { label: "male", value: "0" },
+  { label: "famale", value: "1" },
+];
 
 const SignUpForm = () => {
   /*  const savedData = {
@@ -68,29 +73,8 @@ const SignUpForm = () => {
         label="Password Confirmation"
         type="password"
       />
-      <div className="formControl">
-        <input
-          type="radio"
-          id="0"
-          name="gender"
-          value="0"
-          onChange={formik.handleChange}
-          checked={formik.values.gender === "0"}
-        />
-        <label htmlFor="0">Male</label>
-        <input
-          type="radio"
-          id="1"
-          name="gender"
-          value="1"
-          onChange={formik.handleChange}
-          checked={formik.values.gender === "1"}
-        />
-        <label htmlFor="1">Famale</label>
-        {formik.errors.gender && formik.touched.gender && (
-          <div className="error">{formik.errors.gender}</div>
-        )}
-      </div>
+      <RadioInput formik={formik} radioOptions={radioOptions} name="gender" />
+
       <button type="submit" disabled={!formik.isValid}>
         Submit
       </button>
